9e58818a40622a7d7eaeddb542ee50d975de1812
Saksham Jain <71127462+saksham-jain1@users.noreply.github.com>
2022-06-18 11:40:18 +0530

    Add files via upload

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaTrash } from "react-icons/fa";
 
+
 const Todo = (props) => {
     /*const [editable, setEditable] = useState(false)
     const edit = () => {
@@ -9,8 +10,21 @@ const Todo = (props) => {
          onClick={edit}
     }*/
     return (
-        <li className='list-group-item p-2 m-1' id={props.data.id} style={{fontSize:'24px'}}>&ensp;{props.data.task}
-        <button className='btn p-2 ' id={props.data.id} onClick={props.deleteTask} style={{borderRadius:'50px',float:'right'}}><FaTrash size={26} /></button></li>
+        <table>
+        <tr>
+        <td>
+            <input type="checkbox" name="complete" style={{height:'25px', width:'25px' }} onChange={props.changestatus} id={props.data.id} checked={props.data.complete} /> 
+        </td>
+        <td style={{width:'100%'}}>
+            <li className='list-group-item p-2 m-1' id={props.data.id} style={{fontSize:'18px', textDecorationLine: props.data.complete ?'line-through' : 'none'}}>
+            {props.data.task}
+            </li>
+        </td>
+        <td>
+        <button className='btn p-2 ' id={props.data.id} onClick={props.deleteTask} style={{borderRadius:'50px',float:'right'}}><FaTrash size={26} /></button>
+        </td>
+        </tr>
+        </table>
     )
 }
 
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,50 +1,134 @@
-import React , { useState , useRef } from 'react'
+import React, { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
-import Todo from './todo';
+import Todo from "./todo";
+
+const localstorage = () => {
+  const list = localStorage.getItem("data");
+  if (list) return JSON.parse(list);
+  return [];
+};
 
 const TodoList = () => {
-    const [task,setTask] = useState('');
-    const [index,setIndex] = useState(1);
-    const [data, setData] = useState([]);
-    const deleteData = (e) =>
-    {
-        setData(data.filter((curr) => {
-            if(curr.id==e.currentTarget.id)
-            {
-                return '';
-            }
-            else
-            {
-                return curr;
-            }
-    }));
-    }
-    const addTask = () =>{
-        if(task)
-        {
-            setData([...data,{id:index,task:task}]);
-            setTask('');
-            setIndex(index+1);
+  const [task, setTask] = useState("");
+  const [index, setIndex] = useState(localStorage.getItem("index"));
+  const [data, setData] = useState(localstorage());
+  const deleteData = (e) => {
+    setData(
+      data.filter((curr) => {
+        if (curr.id == e.currentTarget.id) {
+          return "";
+        } else {
+          return curr;
         }
-        else
-        {
-            alert('Enter the task');
+      })
+    );
+  };
+  const changestatus = (e) => {
+    setData(
+      data.filter((curr) => {
+        if (curr.id == e.currentTarget.id) {
+          curr.complete = !curr.complete;
+          return curr;
+        } else {
+          return curr;
         }
+      })
+    );
+  };
+  const addTask = async () => {
+    if (task) {
+      // console.log(task);
+      // try{
+      //     console.log("hh");
+      //     const url =`http://localhost:8000/notes?note=${task}&title`;
+      //     const response = await fetch(url,{
+      //         method: "POST"
+      //     });
+      //     const resjson = await response.json();
+      //     console.log(resjson);
+      // }
+      // catch (e)
+      // {
+      //     console.log(e);
+      // }
+
+      setData([{ id: index, task: task, complete: "" }, ...data]);
+      setTask("");
+      setIndex(index * 1 + 1);
+    } else {
+      alert("Enter the task");
     }
-    return (
-        <div className='card col-md-6 container-fluid align-center bg-light shadow-lg my-5 p-0' style={{borderRadius:'25px'}}>
-        <h2 className='text-center bg-info m-0 p-3' style={{borderRadius:'25px 25px 0px 0px'}}>To-Do List</h2>
-        <div className="input-group justify-content-center my-5">
-        <input type="text" className='field-input col-7' onChange={(e)=>setTask(e.target.value)} value={task} style={{borderStyle: 'hidden',borderRadius:'25px'}} placeholder='Add Task' /> 
-        <button className='btn btn1 p-2 mx-3' onClick={addTask} style={{borderRadius:'50px'}}><FaPlus size={50} /></button>
-        </div>
-            <ul className='list-group list-group-flush m-5' style={{listStyleType:'none'}}>
-                {
-                    data.map((curr,i) =>{ return (<Todo key={curr.id} data={curr} deleteTask={deleteData} />) })
-                } 
-            </ul>
-        </div>
-    )
-}
+  };
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+    localStorage.setItem("index", index);
+  }, [data]);
+
+  return (
+    <div
+      className="card col-md-6 container-fluid align-center bg-light shadow-lg my-5 p-0"
+      style={{ borderRadius: "25px" }}
+    >
+      <h2
+        className="text-center bg-info m-0 p-3"
+        style={{ borderRadius: "25px 25px 0px 0px" }}
+      >
+        To-Do List
+      </h2>
+      <div className="input-group justify-content-center my-5">
+        <input
+          type="text"
+          className="field-input col-7"
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+          style={{ borderStyle: "hidden", borderRadius: "25px" }}
+          placeholder="Add Task"
+        />
+        <button
+          className="btn btn1 p-2 mx-3"
+          onClick={addTask}
+          style={{ borderRadius: "50px" }}
+        >
+          <FaPlus size={50} />
+        </button>
+      </div>
+      <hr /> 
+      <h4 className="text-center">Pending Tasks</h4>
+      <ul
+        className="list-group list-group-flush m-5"
+        style={{ listStyleType: "none" }}
+      >
+        {data.map((curr, i) => {
+            if(curr.complete==false)
+          return (
+            <Todo
+              key={curr.id}
+              changestatus={changestatus}
+              data={curr}
+              deleteTask={deleteData}
+            />
+          );
+        })}
+      </ul>
+      <h4 className="text-center">Completed Tasks</h4>
+      <ul
+        className="list-group list-group-flush m-5"
+        style={{ listStyleType: "none" }}
+      >
+        {data.map((curr, i) => {
+            if(curr.complete==true)
+          return (
+            <Todo
+              key={curr.id}
+              changestatus={changestatus}
+              data={curr}
+              deleteTask={deleteData}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default TodoList;
